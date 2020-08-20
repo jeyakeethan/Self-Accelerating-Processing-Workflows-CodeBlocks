61f4d4b44d08017ea5783f08d78961aa82bf388b
unknown <jeyakeethan@gmail.com>
2020-08-20 20:45:01 +0530

    add cu files

--- src/ArrayAdditionModel.cpp
+++ src/ArrayAdditionModel.cpp
@@ -3,6 +3,18 @@
 
 using namespace std;
 
+#define THREATS_PER_BLOCK 1000
+
+ __global__ void Vector_Addition ( const int *dev_a , const int *dev_b , int *dev_c)
+{
+      //Get the id of thread within a block
+      unsigned short tid = threadIdx.x ;
+
+      if ( tid < THREATS_PER_BLOCK ) // check the boundry condition for the threads
+            dev_c [tid] = dev_a[tid] + dev_b[tid] ;
+
+}
+
 class ArrayAdditionModel: public ComputationalModel
 {
     public:
@@ -25,7 +37,7 @@ class ArrayAdditionModel: public ComputationalModel
         virtual void GPUImplementation(){
             cout << "Hello GPU works!" << endl;
             //Device array
-            /*int *dev_a , *dev_b, *dev_c ;
+            int *dev_a , *dev_b, *dev_c;
             //Allocate the memory on the GPU
             cudaMalloc((void **)&dev_a , N*sizeof(int));
             cudaMalloc((void **)&dev_b , N*sizeof(int));
@@ -38,8 +50,8 @@ class ArrayAdditionModel: public ComputationalModel
             //Copy back to Host array from Device array
             cudaMemcpy(localC , dev_c , N*sizeof(int) , cudaMemcpyDeviceToHost);
             //Free the Device array memory
-            cudaFree (dev_a) ;
-            cudaFree (dev_b) ;
-            cudaFree (dev_c) ;*/
+            cudaFree (dev_a);
+            cudaFree (dev_b);
+            cudaFree (dev_c);
         }
 };
