d31e0af5b59f9831605a298960203003c84daefe
unknown <jeyakeethan@gmail.com>
2020-08-20 19:25:01 +0530

    time changed

--- src/ComputationalModel.cpp
+++ src/ComputationalModel.cpp
@@ -39,11 +39,11 @@ void ComputationalModel::execute(int mode)
 void ComputationalModel::updateResults(clock_t start, clock_t stop, int processor){
     // To a async function
     clock_t delay = stop - start;
-    float time = (float)delay/CLOCKS_PER_SEC;
+    float time = (float)delay/CLOCKS_PER_SEC*1000000;
     if(processor==0){
-        cout << "CPU Time: " << time << " ms" << endl;
+        cout << "CPU Time: " << time << " ns" << endl;
     } else {
-        cout << "GPU Time: " << time << " ms" << endl;
+        cout << "GPU Time: " << time << " ns" << endl;
     }
 
     // cout << tCPU <<","<<tGPU << endl << endl;
